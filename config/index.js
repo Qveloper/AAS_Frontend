d6d95e9097e1cde9bf13c44134e1dd77071a6df3
김규범(Kyu-Beom Kim)/플랫폼_Tech1그룹/SK <kyubeom_kim@sk.com>
2019-11-14 18:14:22 +0900

    Deploy 환경을 위해 proxy 변경

--- config/index.js
+++ config/index.js
@@ -1,8 +1,9 @@
-'use strict'
+
+
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   dev: {
@@ -12,7 +13,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3000/api',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '',
@@ -48,15 +49,15 @@ module.exports = {
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
-    cssSourceMap: true
+    cssSourceMap: true,
   },
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../AAS_Backend/public/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../AAS_Backend/public'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
@@ -79,6 +80,6 @@ module.exports = {
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
-  }
-}
+    bundleAnalyzerReport: process.env.npm_config_report,
+  },
+};
